c079860b14c100079796a752f12e5515fd7a4d91
mdshakil04 <m.shakil04@yahoo.com>
2023-11-28 22:27:58 +0600

    All Classes functionality added

--- index.js
+++ index.js
@@ -24,7 +24,16 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
+    // ------------------------------------------------------------
+    // Collect data from database
     await client.connect();
+    const classCollection = client.db("LwLDB").collection("Classes")
+
+    app.get('/class', async(req, res) =>{
+      const result = await classCollection.find().toArray();
+      res.send(result);
+    } )
+    // ----------------------------------------------------------
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
